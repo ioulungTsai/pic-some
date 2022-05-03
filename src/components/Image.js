82daedf2e75226e6a5b6633beaf606128d5ee628
brandonTsai <brandonya125@gmail.com>
2022-05-03 01:02:31 -0700

    image propTypes

--- src/components/Image.js
+++ src/components/Image.js
@@ -1,5 +1,6 @@
 import React, {useState, useContext} from "react"
 import {Context} from "../Context"
+import PropTypes from "prop-types"
 
 function Image({className, img}) {
   const [hovered, setHovered] = useState(false)
@@ -26,6 +27,15 @@ function Image({className, img}) {
       {cartIcon}
     </div>
   )
+}
+
+Image.propTypes = {
+  className: PropTypes.string,
+  img: PropTypes.shape({
+    id: PropTypes.string.isRequired,
+    url: PropTypes.string.isRequired,
+    isFavorite: PropTypes.bool
+  })
 }
 
 export default Image
